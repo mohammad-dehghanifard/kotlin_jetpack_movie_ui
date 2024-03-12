8a0f8c67225f983b377ee0524f955da2ed1e887e
mohammad <m.deheghanifard@gmail.com>
2024-03-12 08:54:54 +0330

    impl slider model class

--- app/src/main/java/ir/dehghanifard/kotlin_jetpack_movie_ui/MainActivity.kt
+++ app/src/main/java/ir/dehghanifard/kotlin_jetpack_movie_ui/MainActivity.kt
@@ -5,13 +5,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import dev.chrisbanes.snapper.ExperimentalSnapperApi
+import dev.chrisbanes.snapper.rememberSnapperFlingBehavior
 import ir.dehghanifard.kotlin_jetpack_movie_ui.ui.theme.AppDark
 import ir.dehghanifard.kotlin_jetpack_movie_ui.ui.theme.AppGray
 import ir.dehghanifard.kotlin_jetpack_movie_ui.ui.theme.Kotlin_jetpack_movie_uiTheme
@@ -33,12 +41,27 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalSnapperApi::class)
 @Composable
 fun MainView() {
+    val lazyState = rememberLazyListState()
+    val sliderHeight = 340.dp
     Box(modifier = Modifier
         .fillMaxSize()
         .background(AppDark)) {
+        Column {
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .height(sliderHeight)) {
+                LazyRow(
+                    state = lazyState,
+                    flingBehavior = rememberSnapperFlingBehavior(lazyState),
+                    modifier = Modifier.fillMaxWidth().height(sliderHeight)
+                ) {
 
+                }
+            }
+        }
     }
 }
 
---
+++ app/src/main/java/ir/dehghanifard/kotlin_jetpack_movie_ui/models/Slider.kt
@@ -0,0 +1,5 @@
+package ir.dehghanifard.kotlin_jetpack_movie_ui.models
+
+import androidx.annotation.DrawableRes
+
+data class Slider(@DrawableRes val image : Int)
