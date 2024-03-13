d5767949dcc66c252cb58b62b799e75bebdecf03
mohammad <m.deheghanifard@gmail.com>
2024-03-13 09:49:48 +0330

    impl NavigationItems object

---
+++ app/src/main/java/ir/dehghanifard/kotlin_jetpack_movie_ui/data/NavigationItems.kt
@@ -0,0 +1,11 @@
+package ir.dehghanifard.kotlin_jetpack_movie_ui.data
+
+import ir.dehghanifard.kotlin_jetpack_movie_ui.R
+import ir.dehghanifard.kotlin_jetpack_movie_ui.models.NavItem
+
+object NavigationItems {
+    val Home = NavItem(icon = R.drawable.ic_home,"Home")
+    val Video = NavItem(icon = R.drawable.ic_video,"Video")
+    val Favorite = NavItem(icon = R.drawable.ic_heart,"Favorite")
+    val User = NavItem(icon = R.drawable.ic_user,"Profile")
+}
